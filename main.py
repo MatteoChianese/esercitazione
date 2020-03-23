5ab6a5d1e438d9ac0e244847e722ea62770f31eb
Matteo Chianese <matteochianese.vab10@gmail.com>
2020-03-23 10:45:34 +0100

    modificato main

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ b = input ("Quanti anni hai? ")
 d = input("Dove vivi? ")
 print("Ciao {} di {} anni di {}!".format(a, b, d))
 c = input("Come stai? ")
+if c == "Bene":
+  print("Sono contento!")
+elif c == "Male":
+  print("Mi dispiace!")
+else:
+  print("Come dici?")
